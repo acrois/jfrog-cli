93429f937f405365f0db547d5ac3c67e256b763b
Aaron Croissette <aaronc@jfrog.com>
2025-03-11 00:07:24 -0400

    feat: latest BoolT to disable auto copy latest to prevent parallel CI builds from conflicting and support virtual default deployment repos

--- utils/cliutils/commandsflags.go
+++ utils/cliutils/commandsflags.go
@@ -600,7 +600,9 @@ const (
 	lcExcludeRepos       = lifecyclePrefix + ExcludeRepos
 
 	// plugin flags
-	PluginPublishForce = "plugin-publish-" + Force // alias to force, different description
+	Latest              = "latest"
+	PluginPublishForce  = "plugin-publish-" + Force // alias to force, different description
+	PluginPublishLatest = "plugin-publish-" + Latest
 )
 
 var flagsMap = map[string]cli.Flag{
@@ -1265,6 +1267,10 @@ var flagsMap = map[string]cli.Flag{
 	PluginPublishForce: cli.BoolFlag{
 		Name:  Force,
 		Usage: "[Default: false] Set to true to overwrite existing files and ignore failed architectures.` `",
+	},
+	PluginPublishLatest: cli.BoolTFlag{
+		Name:  Latest,
+		Usage: "[Default: true] Copy the built plugin to the latest directory automatically after publishing.` `",
 	},
 	Verbose: cli.BoolFlag{
 		Name:  Verbose,
@@ -2086,6 +2092,7 @@ var commandFlags = map[string][]string{
 		BuildName, BuildNumber, module, Project,
 		// publish-specific flags
 		PluginPublishForce,
+		PluginPublishLatest,
 		// general re-used
 		symlinks,
 	},
